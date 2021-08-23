61f553a56e24033f4bef94004891c82a5a561554
M Hughes <mhughes2011@gmail.com>
2021-08-23 15:50:45 -0700

    created config folder and initialed the db connection

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI'); //This is utilizing config package and can grab anything in that file.  Setting it equal to the db.
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true});
+
+        console.log('MongoDB connected...');
+    } catch(err) {
+        console.error(err.message);
+        //Exit process (connecting to db) with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
