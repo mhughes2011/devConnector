4e34f06b1cbb0bdc52c1867954cfb39a0a5f2167
M Hughes <mhughes2011@gmail.com>
2021-08-26 08:00:31 -0700

    added the login post route with token verification

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,17 +1,76 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {body, validationResult} = require('express-validator');
 
 const User = require('../../models/User');
 
 // @route   GET api/auth
-// @desc    Test route
+// @desc    Finds a user and returns their info
 // @access  Public
 router.get('/', auth, async (req, res) => {
     try{
         // The -password string removes the password from being sent back in the response.
         const user = await User.findById(req.user.id).select('-password');
         res.json(user);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post('/',
+[ 
+    //The next three lines are express validator middleware functions to check for validation errors.  Easy way to handle validation.
+    body('email', 'Please include a valide email').isEmail(),
+    body('password', 'Password is required').exists()
+],  
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body;
+
+    try {
+        // See if the user exists
+        let user = await User.findOne({email: email});
+        if(!user) {
+            return res.status(400).json({ errors: [{msg: 'Email or password incorrect'}]});
+            //Above statement needs the return keyword to allow headers to be resent because I'm resending the status code
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.status(400).json({ errors: [{msg: 'Email or password incorrect'}]});
+            //Above statement needs the return keyword to allow headers to be resent because I'm resending the status code
+        }
+
+        // Return JSON web token
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            {expiresIn: 360000},
+            (err, token) => {
+                if(err) throw err;
+                res.json({token})
+            }
+        );
+
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server error');
