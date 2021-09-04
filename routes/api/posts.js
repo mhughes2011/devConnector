19ddfc00bc7da456ea00b3a738b856f2289143bf
M Hughes <mhughes2011@gmail.com>
2021-09-04 10:10:31 -0700

    added a get specific post by id and delete that post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -38,4 +38,72 @@ router.post('/', [auth, [
     }
 });
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({date: -1});
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   GET api/posts/:id
+// @desc    Get a post by the id
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(404).json({msg: 'Post not found'});
+        }
+
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        // The below checks to make sure the user submits something in the form of an ObjectId
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({msg: 'Post not found'});
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // The below checks to make sure the user submits something in the form of an ObjectId
+        if(!post) {
+            return res.status(404).json({msg: 'Post not found'});
+        }
+
+        // Check to see if user can delete the post.  Add toString() method because the first is an object and the second is a string.  post.user is the owner of the post and req.user is the person logged in
+        if(post.user.toString() !== req.user.id) {
+            return res.status(401).json({msg: 'User not authorized to delete'})
+        }
+
+        await post.remove();
+
+        res.json({msg: 'Post removed'});
+    } catch (err) {
+        console.error(err.message);
+        // The below checks to make sure the user submits something in the form of an ObjectId
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({msg: 'Post not found'});
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+
+
+
 module.exports = router;
