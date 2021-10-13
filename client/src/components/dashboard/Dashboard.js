ad7b165948991fd7fb045e3ea7ac17ab905c823c
M Hughes <mhughes2011@gmail.com>
2021-10-12 18:04:29 -0700

    got spinner gif to work whenever loading is true

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -14,7 +14,9 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}
         <h1 className='large text-primary'>
             Dashboard
         </h1>
-        <p className='lead'><i className='fas fa-user'></i>Welcome {user && user.name}</p>
+        <p className='lead'>
+            <i className='fas fa-user'></i>Welcome {user && user.name}
+        </p>
     </Fragment>
 }
 
