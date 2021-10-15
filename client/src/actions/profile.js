649a6d5bc9382da1ce7e0850a234ccc1609997b7
M Hughes <mhughes2011@gmail.com>
2021-10-14 21:52:45 -0700

    created the action to handle creating or updating a profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,7 +6,7 @@ import {
     PROFILE_ERROR
 } from './types'
 
-// aGet current user's profile
+// Get current user's profile
 export const getCurrentProfile = () => async dispatch => {
     try {
         const res = await axios.get('/api/profile/me');
@@ -16,6 +16,41 @@ export const getCurrentProfile = () => async dispatch => {
             payload: res.data
         });
     } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+}
+
+// Create or update a profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res = await axios.post('/api/profile', formData, config)
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'))
+
+        if(!edit) {
+            history.push('/dashboard')
+        }
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+        
         dispatch({
             type: PROFILE_ERROR,
             payload: {msg: err.response.statusText, status: err.response.status}
