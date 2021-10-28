5137578f84ae9a7255823bd1863133580bfed5ad
M Hughes <mhughes2011@gmail.com>
2021-10-27 19:36:24 -0700

    created and implemented the profile experience component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import {connect} from 'react-redux'
 import Spinner from '../layout/Spinner'
 import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
+import ProfileExperience from './ProfileExperience'
 import {getProfileById} from '../../actions/profile'
 
 const Profile = ({match, getProfileById, profile: {profile, loading}, auth}) => {
@@ -25,6 +26,14 @@ const Profile = ({match, getProfileById, profile: {profile, loading}, auth}) => 
                 <div class="profile-grid my-1">
                     <ProfileTop profile={profile} />
                     <ProfileAbout profile={profile} />
+                    <div className="profile-exp bg-white p-2">
+                        <h2 className="text-primary">Experience</h2>
+                        {profile.experience.length > 0 ? (<Fragment>
+                            {profile.experience.map(experience => (
+                                <ProfileExperience key={experience._id} experience={experience} />
+                            ))}
+                        </Fragment>) : (<h4>No experience credentials</h4>)}
+                    </div>
                 </div>
             </Fragment>}
         </Fragment>
