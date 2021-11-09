69fc6c23cd36b988cc2937c4796cc0e77ab227e9
M Hughes <mhughes2011@gmail.com>
2021-11-08 19:33:18 -0800

    fixed issues with like and unlike on clicks

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {setAlert} from './alert'
+// import {setAlert} from './alert'
 import {
     GET_POSTS,
     POST_ERROR,
@@ -15,7 +15,6 @@ export const getPosts = () => async dispatch => {
             type: GET_POSTS,
             payload: res.data
         })
-        dispatch(setAlert('Posts are being retrieved...'))
     } catch (err) {
         dispatch({
             type: POST_ERROR,
@@ -25,9 +24,9 @@ export const getPosts = () => async dispatch => {
 }
 
 // Add like
-export const addLike = postId => async dispatch => {
+export const addLike = id => async dispatch => {
     try {
-        const res = await axios.put(`/api/posts/like/${postId}`)
+        const res = await axios.put(`/api/posts/like/${id}`)
 
         dispatch({
             type: UPDATE_LIKES,
@@ -42,9 +41,9 @@ export const addLike = postId => async dispatch => {
 }
 
 // Remove like
-export const removeLike = postId => async dispatch => {
+export const removeLike = id => async dispatch => {
     try {
-        const res = await axios.put(`/api/posts/unlike/${postId}`)
+        const res = await axios.put(`/api/posts/unlike/${id}`)
 
         dispatch({
             type: UPDATE_LIKES,
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 import {Moment} from 'react-moment'
 import {connect} from 'react-redux'
+import {addLike, removeLike} from '../../actions/post'
 
-const PostItem = ({auth, post: {
+const PostItem = ({addLike, removeLike, auth, post: {
     _id, 
     text, 
     name, 
@@ -30,13 +31,13 @@ const PostItem = ({auth, post: {
              <p className="post-date">
                 Posted on {date}
             </p>
-            <button type="button" className="btn btn-light">
+            <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
               <i className="fas fa-thumbs-up"></i>{' '}
               {likes.length > 0 && (
                 <span>{likes.length}</span>
               )}
             </button>
-            <button type="button" className="btn btn-light">
+            <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
               <i className="fas fa-thumbs-down"></i>
             </button>
             <Link to={`/post/${_id}`} className="btn btn-primary">
@@ -56,11 +57,13 @@ const PostItem = ({auth, post: {
 
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired
+    auth: PropTypes.object.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps, {})(PostItem)
+export default connect(mapStateToProps, {addLike, removeLike})(PostItem)
