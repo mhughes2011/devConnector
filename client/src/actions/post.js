bfa415052aa48c7db6fe923ba8521329ba4aff56
M Hughes <mhughes2011@gmail.com>
2021-11-08 19:05:06 -0800

    created update likes action and added add and remove like functions

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import {setAlert} from './alert'
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES
 } from './types'
 
 // Get posts
@@ -15,6 +16,40 @@ export const getPosts = () => async dispatch => {
             payload: res.data
         })
         dispatch(setAlert('Posts are being retrieved...'))
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+}
+
+// Add like
+export const addLike = postId => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/like/${postId}`)
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: {id, likes: res.data}
+        })
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+}
+
+// Remove like
+export const removeLike = postId => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/unlike/${postId}`)
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: {id, likes: res.data}
+        })
     } catch (err) {
         dispatch({
             type: POST_ERROR,
