8cef897636bba72eb0916e37748f9e30440c9740
M Hughes <mhughes2011@gmail.com>
2021-11-11 17:00:54 -0800

    built out post component framework and implemented route

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,16 +4,21 @@ import {connect} from 'react-redux'
 import Spinner from '../layout/Spinner'
 import { getPost } from '../../actions/post'
 
-const Post = ({getPost}) => {
+const Post = ({getPost, post: {post, loading}, match}) => {
+    useEffect(() => {
+        getPost(match.params.id)
+    }, [getPost])
+
     return (
         <div>
-            
+            post
         </div>
     )
 }
 
 Post.propTypes = {
-    getPost: PropTypes.func.isRequired
+    getPost: PropTypes.func.isRequired,
+    post: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -39,7 +39,7 @@ const PostItem = ({addLike, removeLike, deletePost, auth, post: {
             <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
               <i className="fas fa-thumbs-down"></i>
             </button>
-            <Link to={`/post/${_id}`} className="btn btn-primary">
+            <Link to={`/posts/${_id}`} className="btn btn-primary">
               Discussion {comments.length > 0 && (
                 <span className='comment-count'>{comments.length}</span>
               )}
