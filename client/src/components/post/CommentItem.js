c9088b4d7af544977dec11dfb968e76e31da8fe3
M Hughes <mhughes2011@gmail.com>
2021-11-14 12:15:10 -0800

    implemented delete comment function

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -2,11 +2,13 @@ import React, {Fragment} from 'react'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import { deleteComment } from '../../actions/post'
 
 const CommentItem = ({
     postId,
     comment: {_id, text, name, avatar, user, date},
-    auth
+    auth,
+    deleteComment
 }) => {
     return (
         <div class="post bg-white p-1 my-1">
@@ -27,6 +29,11 @@ const CommentItem = ({
              <p class="post-date">
                 Posted on {date}
             </p>
+            {!auth.loading && user === auth.user._id && (
+                <button onClick={e => deleteComment(postId, _id)} type='button' className='btn btn-danger'>
+                    <i className='fas fa-times'></i>
+                </button>
+            )}
           </div>
         </div>
     )
@@ -35,11 +42,12 @@ const CommentItem = ({
 CommentItem.propTypes = {
     postId: PropTypes.number.isRequired,
     comment: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired
+    auth: PropTypes.object.isRequired,
+    deleteComment: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps, {})(CommentItem)
+export default connect(mapStateToProps, {deleteComment})(CommentItem)
