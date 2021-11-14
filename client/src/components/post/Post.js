0578376cabf5636d0d539dbcf31957665d2987b2
M Hughes <mhughes2011@gmail.com>
2021-11-14 12:11:01 -0800

    implemented component to be able ot see comments on posts

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import { getPost } from '../../actions/post'
 
 import PostItem from '../posts/PostItem'
 import CommentForm from './CommentForm'
+import CommentItem from './CommentItem'
 import { Link } from 'react-router-dom'
 
 const Post = ({getPost, post: {post, loading}, match}) => {
@@ -20,6 +21,11 @@ const Post = ({getPost, post: {post, loading}, match}) => {
             </Link>
             <PostItem post={post} showActions={false} />
             <CommentForm postId={post._id} />
+            <div className='comments'>
+                {post.comments.map(comment => (
+                    <CommentItem key={comment._id} comment={comment} postId={post._id} />
+                ))}
+            </div>
         </Fragment>
     )
 }
